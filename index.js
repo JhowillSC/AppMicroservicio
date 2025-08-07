5ecba8de49b42323a94469529e0989a5f954d3c6
Jhonatan Oro <jhonatan_oro@usmp.pe>
2025-08-06 20:50:18 -0500

    Creando la vista del aplicativo

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
-const express = require('express');
-const axios = require('axios');
-const app = express();
+const express = require("express");
+const axios = require("axios");
+const path = require("path");
 
-// Ruta opcional para que / no dé error
-app.get('/', (req, res) => {
-    res.send('Microservicio funcionando. Usa /convertir?cantidad=100&moneda=USD');
+const app = express();
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // importante para recibir JSON si es necesario
+
+// Página principal
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-app.get('/convertir', async (req, res) => {
-    const cantidad = req.query.cantidad || 100;
-    const moneda = req.query.moneda || 'USD';
+// API de conversión (devuelve solo JSON)
+app.post("/convertir", async (req, res) => {
+  const { cantidad, monedaOrigen, monedaDestino } = req.body;
 
-    try {
-        const response = await axios.get(`https://convertidormonedaz-bmffhfdxfwdebvdc.brazilsouth-01.azurewebsites.net/api/convertirMoneda?cantidad=${cantidad}&monedaDestino=${moneda}`);
-        res.json(response.data);
-    } catch (error) {
-        console.error('Error al llamar a la Azure Function:', error.message);
-        res.status(500).send("Error al convertir moneda.");
-    }
+  try {
+    const response = await axios.get(
+      `https://convertidormonedaz-bmffhfdxfwdebvdc.brazilsouth-01.azurewebsites.net/api/convertirMoneda?cantidad=${cantidad}&monedaDestino=${monedaDestino}`
+    );
+    const resultado = response.data.resultado;
+
+    res.json({
+      exito: true,
+      mensaje: `Conversión realizada con éxito`,
+      resultado: `${cantidad} ${monedaOrigen} equivale a ${resultado} ${monedaDestino}`,
+    });
+  } catch (error) {
+    res.json({
+      exito: false,
+      mensaje: "Error al convertir moneda.",
+    });
+  }
 });
 
 app.listen(3000, () => {
-    console.log('Microservicio escuchando en http://localhost:3000');
+  console.log("Microservicio escuchando en http://localhost:3000");
 });
